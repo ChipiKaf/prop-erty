2c859c875843743e3a1ae984f59f7af03e5924d9
Chipili Kafwilo <ckafwilo@gmail.com>
2024-07-06 23:15:12 +0200

    feat: :sparkles: Added navbar

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  active: boolean = false;
+
   constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.active);
+  }
+
+  toggleMenu() {
+    this.active = !this.active;
+    console.log(this.active);
+  }
 }
