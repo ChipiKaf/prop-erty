0f72d9f57ad4d5b2c6e2177fd9826a1c9bd7c806
Chipili Kafwilo <ckafwilo@gmail.com>
2024-12-25 20:56:10 +0200

    fix: Fix memory leaks by cleaning up threejs resources

--- Frontend/src/app/authentication/login/login.component.ts
+++ Frontend/src/app/authentication/login/login.component.ts
@@ -5,18 +5,14 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertifyService } from '../../services/alertify.service';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormCardComponent } from '../../form-card/form-card.component';
 import * as THREE from 'three';
-import {
-  DRACOLoader,
-  GLTFLoader,
-  OrbitControls,
-} from 'three/examples/jsm/Addons.js';
+import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 @Component({
   selector: 'app-login',
@@ -29,18 +25,16 @@ export class LoginComponent implements OnInit, OnDestroy {
   @ViewChild('entry', { static: true })
   canvasContainer!: ElementRef<HTMLDivElement>;
   // Render state
-  private scene!: THREE.Scene;
-  private camera!: THREE.PerspectiveCamera;
-  private renderer!: THREE.WebGLRenderer;
+  private scene: THREE.Scene | null = null;
+  private camera: THREE.PerspectiveCamera | null = null;
+  private renderer: THREE.WebGLRenderer | null = null;
 
   // Loaders
-  private gltfLoader!: GLTFLoader;
-  private dracoLoader!: DRACOLoader;
+  private gltfLoader: GLTFLoader | null = null;
+  private dracoLoader: DRACOLoader | null = null;
 
-  // Controls
-  private controls!: OrbitControls;
-  private cube!: THREE.Mesh;
-  private city!: THREE.Mesh;
+  // model
+  private city!: THREE.Group<THREE.Object3DEventMap>;
 
   // Parallax state
   private oldX: number = 0;
@@ -49,6 +43,9 @@ export class LoginComponent implements OnInit, OnDestroy {
   private cameraOffsetCurrent = new THREE.Vector2(0, 0);
   private cameraOffsetTarget = new THREE.Vector2(0, 0);
   private parallaxStrenght = 5;
+
+  // For cleanup
+  animationId: number | null = null;
 
   constructor(
     private authService: AuthService,
@@ -83,12 +80,12 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.dracoLoader.setDecoderPath('draco/');
     this.gltfLoader.setDRACOLoader(this.dracoLoader);
     this.gltfLoader.load('models/city.glb', (gltf) => {
-      const model = gltf.scene;
+      this.city = gltf.scene;
       gltf.scene.scale.set(1.5, 1.5, 1.5);
       // console.log(model)
       // this.scene.add(model.children);
       // if (!model) return;
-      model.traverse((child) => {
+      this.city.traverse((child) => {
         if ((child as THREE.Mesh).isMesh) {
           const mesh = child as THREE.Mesh;
           mesh.material = new THREE.MeshBasicMaterial({
@@ -97,7 +94,7 @@ export class LoginComponent implements OnInit, OnDestroy {
           });
         }
       });
-      this.scene.add(model);
+      this.scene?.add(this.city);
     });
   }
 
@@ -109,9 +106,32 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   private onWindowResize() {
     const container = this.canvasContainer.nativeElement;
+    if (!this.camera || !this.renderer) return;
     this.camera.aspect = container.offsetWidth / container.offsetHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(container.offsetWidth, container.offsetHeight);
+  }
+
+  private disposeThreeJs() {
+    this.camera = null;
+    this.scene = null;
+
+    this.gltfLoader = null;
+    this.dracoLoader = null;
+
+    this.city.traverse((child) => {
+      if ((child as THREE.Mesh).isMesh) {
+        const mesh = child as THREE.Mesh;
+        mesh.geometry?.dispose();
+
+        if (Array.isArray(mesh.material)) {
+          mesh.material?.forEach((mat) => mat.dispose());
+        } else {
+          mesh.material?.dispose();
+        }
+      }
+      this.renderer?.dispose();
+    });
   }
 
   private addEvents() {
@@ -135,40 +155,30 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.removeEvents();
+
+    if (this.animationId) {
+      cancelAnimationFrame(this.animationId);
+      this.animationId = null;
+    }
+    this.disposeThreeJs();
   }
 
   animate() {
-    requestAnimationFrame(() => this.animate());
+    this.animationId = requestAnimationFrame(() => this.animate());
     this.cameraOffsetCurrent.lerp(this.cameraOffsetTarget, 0.05);
     const offsetX = this.cameraOffsetCurrent.x * this.parallaxStrenght;
     const offsetY = this.cameraOffsetCurrent.y * this.parallaxStrenght;
 
-    this.camera.position.set(
+    this.camera?.position.set(
       this.cameraInitialPos.x + offsetX,
       this.cameraInitialPos.y - offsetY,
       this.cameraInitialPos.z
     );
 
-    this.camera.lookAt(0, 0, 0);
+    this.camera?.lookAt(0, 0, 0);
     // this.controls.update();
     // this.camera.position.z += 0.01;
-    this.renderer.render(this.scene, this.camera);
-  }
-
-  onLogin(loginForm: NgForm) {
-    console.log(loginForm.value);
-    // const token = this.authService.authUser(loginForm.value);
-    // this.authService.authUser(loginForm.value).subscribe(
-    // (response: UserForLogin) => {
-    //     console.log(response);
-    //     const user = response;
-    //     if (user) {
-    //         localStorage.setItem('token', user.token);
-    //         localStorage.setItem('userName', user.userName);
-    //         this.alertify.success('Login Successful');
-    //         this.router.navigate(['/']);
-    //     }
-    // }
-    // );
+    if (this.scene && this.camera)
+      this.renderer?.render(this.scene, this.camera);
   }
 }
