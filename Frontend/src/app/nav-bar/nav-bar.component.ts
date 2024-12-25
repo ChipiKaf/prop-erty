afac52bf1d0164324ade5c72a44c87767fef6c75
Chipili Kafwilo <ckafwilo@gmail.com>
2024-12-25 20:58:54 +0200

    refactor: Move nav items to template as array

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,17 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+
+export enum ListItemTypes {
+  AUTHENTICATED,
+  UNAUTHENTICATED,
+}
+
+export type ListItem = {
+  name: string;
+  url: string;
+  type: ListItemTypes;
+  actions?: () => void;
+};
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
   active: boolean = false;
+  listItems: ListItem[] = [
+    {
+      name: 'Properties',
+      url: '/',
+      type: ListItemTypes.UNAUTHENTICATED,
+    },
+    {
+      name: 'Contact',
+      url: '/',
+      type: ListItemTypes.UNAUTHENTICATED,
+    },
+    {
+      name: 'Logout',
+      url: '/login',
+      type: ListItemTypes.AUTHENTICATED,
+      actions: () => {
+        localStorage.removeItem('token');
+      },
+    },
+  ];
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     console.log(this.active);
@@ -20,5 +53,18 @@ export class NavBarComponent implements OnInit {
   toggleMenu() {
     this.active = !this.active;
     console.log(this.active);
+  }
+
+  isItemVisible(item: ListItem): boolean {
+    return (
+      item.type === ListItemTypes.UNAUTHENTICATED ||
+      (item.type === ListItemTypes.AUTHENTICATED &&
+        !!localStorage.getItem('token'))
+    );
+  }
+
+  navigate(item: ListItem) {
+    if (item.actions) item.actions();
+    this.router.navigate([item.url]);
   }
 }
